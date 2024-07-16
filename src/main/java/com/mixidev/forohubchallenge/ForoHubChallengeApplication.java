21fc0e05f49e29f06aaf0a09f974e25dba8db96c
Ronaldo <rnlprzp@gmail.com>
2024-07-16 03:06:21 -0500

    iniciando

---
+++ src/main/java/com/mixidev/forohubchallenge/ForoHubChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.mixidev.forohubchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForoHubChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mixidev/forohubchallenge/ForoHubChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mixidev.forohubchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
